ca18155b62820fa374790ce9803e6f6be7c8e65f
Marc Dubois <marcdubois@MacBook-de-Marc.local>
2018-11-29 15:22:42 +0100

    HTTPS add

--- src/server/index.js
+++ src/server/index.js
@@ -7,9 +7,9 @@ const express = require('express');
 const app = express();
 
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.marcdubois.fr/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/www.marcdubois.fr/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/www.marcdubois.fr/chain.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
