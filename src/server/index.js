88472c7d3b8203d3c5823381612903bbf7148b04
Marc Dubois <marcdubois@MacBook-de-Marc.local>
2018-11-29 16:20:20 +0100

    redirect no-www to www

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,16 @@ const express = require('express');
 
 const app = express();
 
-// Certificate
+app.all(/.*/, function(req, res, next) {
+  var host = req.header("host");
+  if (host.match(/^www\..*/i)) {
+    next();
+  } else {
+    res.redirect(301, "http://www." + host);
+  }
+});
+
+
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.marcdubois.fr/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/www.marcdubois.fr/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/www.marcdubois.fr/chain.pem', 'utf8');
