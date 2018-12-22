ec480608a840ef012bb1eaa2d8151683637074ba
Marc Dubois <marcdubois71450@gmail.com>
2018-12-22 14:29:00 +0100

    update

--- src/client/components/App/App.js
+++ src/client/components/App/App.js
@@ -7,10 +7,26 @@ export default class App extends Component {
   render() {
 
     return (
-
       <div>
-      <div className="title">Marc Dubois</div>
-      <div className="texte">Le site est en construction.</div>
+        <div className="annecy">
+          <div className="title">Marc Dubois</div>
+          <div className="texte">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          <div className="info">?<div className="info2">Lac d'annecy</div>
+        </div>
+        </div>
+        <div className="experience">
+          <div className="title2">Experience Professionelle</div>
+          <div className="experience-div-total">
+            <div className="experience-div">
+              <div className="experience-title">Stage 2ème DUT</div>
+              <div className="experience-texte">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco labor</div>
+            </div>
+            <div className="experience-div">
+              <div className="experience-title">Animateur enfant</div>
+              <div className="experience-texte">isdjhfniuejsdfhdn,iuzj oehfdn oisehjdf zsejdf iz ejf zse fjis eidjf sike jfd</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
