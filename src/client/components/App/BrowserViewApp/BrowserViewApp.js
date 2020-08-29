85d0d7134d2d4ee30737d119ff8dcf30b8bac449
Marc Dubois <marcdubois@MacBook-Pro-de-Marc-3.local>
2020-08-29 02:54:17 +0200

    clean prod

--- src/client/components/App/BrowserViewApp/BrowserViewApp.js
+++ src/client/components/App/BrowserViewApp/BrowserViewApp.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
 import Classique from './Classique';
-import Dev from './Dev';
-import './BrowserViewApp.css';
-import SettingSVG from './img/setting-icon.svg';
-
-
 
 export default class BrowserViewApp extends Component {
   render() {
     return (
       <div>
-        <div styleName="menu-hover">
-          <div styleName="menu-top-right">
-          <img src={SettingSVG} styleName="setting-icon" alt="Logo Setting" />
-
-            <div>Parametre</div>
-            <div>Mode Sombre</div>
-            <div>Mode Devellopeur</div>
-          </div>
-        </div>
         <Classique />
-        <Dev />
       </div>
     );
   }
--- src/client/components/App/BrowserViewApp/Classique/Reseaux/Reseaux.js
+++ src/client/components/App/BrowserViewApp/Classique/Reseaux/Reseaux.js
@@ -8,8 +8,8 @@ export default class Reseaux extends Component {
       <div styleName="fullreseaux">
         <h2 styleName="title-reseaux">Mes Réseaux</h2>
         <div styleName="main-reseaux">
-          <a href="https://www.instagram.com/marcdubois71/" target="_blank" styleName="instagram"></a>
-          <a href="https://www.linkedin.com/in/marc-dubois-7a30b3137/" target="_blank" styleName="linkedin" target="_blank"></a>
+          <a href="https://www.instagram.com/marc20dubois/" target="_blank" styleName="instagram"></a>
+          <a href="https://www.linkedin.com/in/marc-dubois-7a30b3137/" target="_blank" styleName="linkedin"></a>
           <a href="https://github.com/marcdubois71450" target="_blank" styleName="github"></a>
           <a href="https://www.youtube.com/channel/UCV5t2646kqVwV4GUBehc8qw" target="_blank" styleName="youtube"></a>
         </div>
